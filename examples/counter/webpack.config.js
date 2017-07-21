689c4d7320fb7d86eabbd835033707c1a8b2daee
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2017-07-21 15:03:50 +0100

    Add counter example

---
+++ examples/counter/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: {
+        index: './index.js'
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.one$/,
+                loader: '../../src',
+                options: {
+                    map: {
+                        'text/css': 'style-loader!css-loader!',
+                        javascript: 'babel-loader!'
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
