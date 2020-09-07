e1b636b4d8c435d62d8c96a1fc5ac17e319fcdef
Zauberfisch <zauberfisch@zauberfisch.at>
2020-09-07 05:18:46 +0200

    Switched to cheerio for parsing html. This fixes <template> tags

--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,4 @@
-const parser = require('posthtml-parser');
+const cheerio = require('cheerio');
 
 /**
  * Parse file content (html) and categorize it by tag and type
@@ -7,62 +7,22 @@ const parser = require('posthtml-parser');
  * @returns {object}
  */
 module.exports.default = function (content) {
+    const defaultTagTypes = {
+        script: 'javascript',
+        style: 'text/css',
+        template: 'text/html',
+    }
+    const $ = cheerio.load(`<!DOCTYPE html><html><head></head><body>${content}</body></html>`, {decodeEntities: false});
     let output = [];
-    const nodes = parser(content);
-    nodes.forEach((node, i) => {
-        if (typeof node == 'object' && isCorrectTag(node)) {
-            output.push({
-                id: typeof node.attrs !== 'undefined' && node.attrs.id || `__one_loader_internal__part-${i}`,
-                tag: node.tag,
-                mimeType: getType(node),
-                content: getContent(node),
-            })
-        }
+    $('body > script, body > style, body > template').each(function (i) {
+        const node = $(this);
+        output.push({
+            id: node.attr('id') || `__one_loader_internal__part-${i}`,
+            tag: this.tagName,
+            mimeType: node.attr('type') || defaultTagTypes[this.tagName],
+            content: node.html(),
+        })
     });
     return output;
 }
 
-/**
- * Get code content of given tag (node)
- * @param {object} node
- * @returns {string}
- */
-function getContent(node) {
-    return (node.content || []).join(' ');
-}
-
-/**
- * Get tag type property
- * @param {object} node
- * @returns {string}
- */
-function getType(node) {
-    const tagLoaders = {
-        script: 'javascript',
-        style: 'text/css'
-    };
-
-    if(typeof node.attrs == 'undefined' || typeof node.attrs.type == 'undefined')
-        return tagLoaders[node.tag];
-
-    return node.attrs.type;
-}
-
-/**
- * Verify tag is either `script` or `style`
- *
- * @param {object} node
- * @returns {boolean}
- */
-function isCorrectTag(node) {
-    return node.tag === 'script' || node.tag === 'style' || node.tag === 'template';
-}
-
-/*
- * Export private functions for testing purposes
- */
-if (process.env.NODE_ENV === 'test') {
-    module.exports.getContent = getContent;
-    module.exports.getType = getType;
-    module.exports.isCorrectTag = isCorrectTag;
-}
